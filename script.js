35b695669cadd39529f8a9068c261033962b8ccb
Kumudini Gandesiri <kumsiri125@gmail.com>
2025-01-31 19:17:32 -0800

    updated portfolio with latest changes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["Python Developer", "Web Developer"],
+    strings: ["Data Analyst", "Data Enthusiastic"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
